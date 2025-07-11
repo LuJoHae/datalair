17e404562150eb3ad7c7206a2e7a9b6b88a97aba
Lukas Jonathan Haeuser <lhaeuser@ethz.ch>
2025-07-11 14:43:06 +0200

    Readded UUIDs.

--- datalair/__init__.py
+++ datalair/__init__.py
@@ -18,6 +18,22 @@ import requests
 from tqdm import tqdm
 
 
+class UUID(str):
+    _pattern = re.compile(r'^[0-9a-fA-F]{16}$')
+
+    def __new__(cls, value):
+        if not isinstance(value, str):
+            raise TypeError("Hex16String must be created from a string")
+        if not cls._pattern.match(value):
+            raise ValueError(f"'{value}' is not a valid 16-digit hexadecimal string")
+        return str.__new__(cls, value)
+
+
+def generate_random_uuid() -> UUID:
+    return UUID(''.join(choices('0123456789abcdef', k=16)))
+
+
+
 class Dataset(ABC):
 
     def __init__(self):
